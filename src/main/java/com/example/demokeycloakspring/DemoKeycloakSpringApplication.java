3ab2bb58376a83fa1949b929e6ce8140998f3cfe
Hassan Ibrahim <h.ibrahim1995@hotmail.com>
2023-12-08 16:44:02 +0100

    add bean

--- src/main/java/com/example/demokeycloakspring/DemoKeycloakSpringApplication.java
+++ src/main/java/com/example/demokeycloakspring/DemoKeycloakSpringApplication.java
@@ -2,12 +2,19 @@ package com.example.demokeycloakspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoKeycloakSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoKeycloakSpringApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
